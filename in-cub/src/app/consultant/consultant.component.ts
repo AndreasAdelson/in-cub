98703030cd8e805124ba76c65944239d509aab8c
Andreas <andreasadelson@gmail.com>
2019-02-06 09:40:18 +0100

    Push intermediaire

--- in-cub/src/app/consultant/consultant.component.ts
+++ in-cub/src/app/consultant/consultant.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../store.service';
+import { Consultant } from '../store';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-consultant',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultant.component.css']
 })
 export class ConsultantComponent implements OnInit {
+  consultants: Array<Consultant> = [];
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
 
   ngOnInit() {
+    this.storeService.consultantList().subscribe(
+      x => {
+        console.log(x);
+        x.forEach(consultant => {
+          this.consultants.push(consultant);
+        }); 
+      } ,
+      err => console.log('The observable on the consultant page get an error ', err),
+      () => console.log("The observable on consultant page complete")
+    )
+    console.log(this.consultants);
   }
 
 }
