72dc24b692f8e258ac73b559ab362f722d7e46b1
Andreas <andreasadelson@gmail.com>
2019-02-06 11:14:35 +0100

    Ajout de tableau des startups

--- in-cub/src/app/startup/startup.component.ts
+++ in-cub/src/app/startup/startup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../store.service';
 
 @Component({
   selector: 'app-startup',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartupComponent implements OnInit {
 
-  constructor() { }
+  startups;
+  constructor(private storeService: StoreService) { }
 
   ngOnInit() {
+    this.storeService.startUpList().subscribe(
+      x => {
+        this.startups = x
+        console.log(this.startups)
+      },
+      err => console.log("The observable on startup page get an error : ", err),
+      () => console.log("The observable on startup page complete")
+    )
   }
 
 }
